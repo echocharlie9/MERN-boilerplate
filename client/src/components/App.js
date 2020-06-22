0414ab20d2fa4e9770a645f4d97ac94386154294
Colby Anderson <colby-anderson@colbys-mbp.home>
2020-06-22 00:01:32 -0700

    df

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Suspense } from 'react';
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+  return (
+<p>
+  hello
+</p>
+  );
+}
+
+export default App;
---
+++ server/routes/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { User } = require("../models/User");
+
+
+
+
+
+
+
+
+
+
+//=================================
+//             User
+//=================================
+
+router.get('/profile', (req, res) => {
+})
+
+
+module.exports = router;
